8c71cb81fcb60bc1edd490e41d6c923b987bfbda
Berk Erdemoglu <69506422+berkerdemoglu@users.noreply.github.com>
2020-09-03 18:02:39 +0300

    Deleted random stuff

--- data_visualysis/data_main.py
+++ data_visualysis/data_main.py
@@ -1,9 +1,4 @@
 from data_visualysis import DataVisualysis
 
 # Create a DataVisualysis object and start running commands!
-data_modeler = DataVisualysis('saved_files/eczane_agustos2020.json')
-data_modeler.show_results()
-# data_modeler.scatter_dataset('viridis')
-# data_modeler.line_graph()
-# data_modeler.compare_datasets([1,2,3,4,5])
-# data_modeler.show_colormaps()
+data_modeler = DataVisualysis()
