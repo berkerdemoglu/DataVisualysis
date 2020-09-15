d1fcc2172fb4bb7b30a82beb126f14f017e3101b
Berk Erdemoglu <69506422+berkerdemoglu@users.noreply.github.com>
2020-09-15 14:01:21 +0300

    Uploaded setup files

---
+++ setup/dv-setup-windows.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	// Upgrade it.
+	system("pip install -U --upgrade pip");
+
+	// Install the required modules.
+	system("pip install -U numpy");
+	system("pip install -U requests");
+	system("pip install -U matplotlib");
+	system("pip install -U mplcursors");
+
+	return 0;
+}
